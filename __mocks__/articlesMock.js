efa8794d404a6a1f5eb816ca26fe20c0045a499b
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-10.local>
2017-10-13 15:08:11 +0100

    'modify articlesMock.js'

--- __mocks__/articlesMock.js
+++ __mocks__/articlesMock.js
@@ -1,50 +1,50 @@
  const articles = [
-  { author: null,
-    description: `The type of panels reportedly 
+   { author: null,
+     description: `The type of panels reportedly 
       used on the London apartment complex where at 
       least 30 people died in a fire this week should 
       not be used in high-rise buildings, a 
       brochure from manufacturer shows.`,
-    publishedAt: '2017-06-17T10:43:22Z',
-    title: `Manufacturer issued warning 
+     publishedAt: '2017-06-17T10:43:22Z',
+     title: `Manufacturer issued warning 
       over panels reportedly used on London tower`,
-    url: 'http://www.abc.net.au/news/2017-06-17/8627790',
-    urlToImage: 'http://www.abc.net.au/news/image/8627822-1x1-700x700.jpg'
-  },
-  { author: null,
-    description: `Seven US Navy sailors remain missing in the 
+     url: 'http://www.abc.net.au/news/2017-06-17/8627790',
+     urlToImage: 'http://www.abc.net.au/news/image/8627822-1x1-700x700.jpg'
+   },
+   { author: null,
+     description: `Seven US Navy sailors remain missing in the 
     Pacific Ocean after their US destroyer collided 
     with a container ship off the coast of Japan.`,
-    publishedAt: '2017-06-17T11:55:54Z',
-    title: `Sailors missing after US destroyer 
+     publishedAt: '2017-06-17T11:55:54Z',
+     title: `Sailors missing after US destroyer 
     smashes into merchant ship off Japan`,
-    url: `http://www.abc.net.au/news/2017-06-17/
+     url: `http://www.abc.net.au/news/2017-06-17/
     us-navy-ship-collides-with-merchant-ship-off-japan/8627298`,
-    urlToImage: 'http://www.abc.net.au/news/image/8627894-1x1-700x700.jpg'
-  },
-  { author: null,
-    description: `At least one Afghan soldier 
+     urlToImage: 'http://www.abc.net.au/news/image/8627894-1x1-700x700.jpg'
+   },
+   { author: null,
+     description: `At least one Afghan soldier 
     is killed and several American soldiers 
     wounded in an incident at the Camp 
     Shaheen military base in northern Afghanistan.`,
-    publishedAt: '2017-06-17T14:16:36Z',
-    title: `Afghan soldier killed, several US 
+     publishedAt: '2017-06-17T14:16:36Z',
+     title: `Afghan soldier killed, several US 
     soldiers wounded in incident at Army base`,
-    url: `http://www.abc.net.au/news/2017-06-17
+     url: `http://www.abc.net.au/news/2017-06-17
     /us-soldiers-wounded-afghan-soldier-killed-at-camp-shaheen
     /8627922`,
-    urlToImage: 'http://www.abc.net.au/news/image/3882848-1x1-700x700.jpg'
-  },
-  { author: null,
-    description: `Desperate family and friend of missing 
+     urlToImage: 'http://www.abc.net.au/news/image/3882848-1x1-700x700.jpg'
+   },
+   { author: null,
+     description: `Desperate family and friend of missing 
     Grenfell Tower residents are appealing for help online.`,
-    publishedAt: '2017-06-17T02:25:14Z',
-    title: `London fire families appeal for help in
+     publishedAt: '2017-06-17T02:25:14Z',
+     title: `London fire families appeal for help in
      desperate hunt for missing loved ones`,
-    url: `http://www.abc.net.au/news/2017-06-17
+     url: `http://www.abc.net.au/news/2017-06-17
     /desperate-grenfell-families-take-to-social-media/8627390`,
-    urlToImage: 'http://www.abc.net.au/news/image/8627562-1x1-700x700.jpg'
-  }
-];
+     urlToImage: 'http://www.abc.net.au/news/image/8627562-1x1-700x700.jpg'
+   }
+ ];
 
-export default articles;
+ export default articles;
